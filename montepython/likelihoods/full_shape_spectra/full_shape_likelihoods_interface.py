30a775f5abe812498f9c8d186a38e7d02e82061f
Keir <keirrogers@hotmail.com>
2022-02-18 17:18:13 -0500

    CosmoSIS input using JSON

--- montepython/likelihoods/full_shape_spectra/full_shape_likelihoods_interface.py
+++ montepython/likelihoods/full_shape_spectra/full_shape_likelihoods_interface.py
@@ -1,16 +1,20 @@
 #!python3
 import numpy as np
+import json
 import full_shape_likelihoods as fsl
 from cosmosis.datablock import names
 
 def setup(options):
     """Setup likelihood class."""
     #Load options from block
-    options_names = ['nz', 'z', 'inv_nbar', 'no_wiggle', 'use_P', 'use_Q', 'use_B', 'use_AP', 'data_directory',
-                     'P_measurements', 'B_measurements', 'AP_measurements', 'covmat_file', 'discreteness_weights_file',
-                     'kminP', 'kmaxP', 'kmaxQ', 'kminB', 'kmaxB', 'ksizeB', 'bin_integration_P', 'h_fid', 'Hz_fid',
-                     'DA_fid', 'rdHfid', 'rdDAfid']
-    options_dict = {options_name: options['full_shape_likelihoods', options_name] for options_name in options_names}
+    #options_names = ['nz', 'z', 'inv_nbar', 'no_wiggle', 'use_P', 'use_Q', 'use_B', 'use_AP', 'data_directory',
+    #                 'P_measurements', 'B_measurements', 'AP_measurements', 'covmat_file', 'discreteness_weights_file',
+    #                 'kminP', 'kmaxP', 'kmaxQ', 'kminB', 'kmaxB', 'ksizeB', 'bin_integration_P', 'h_fid', 'Hz_fid',
+    #                 'DA_fid', 'rdHfid', 'rdDAfid']
+    #options_dict = {options_name: options['full_shape_likelihoods', options_name] for options_name in options_names}
+    params_json = options['full_shape_likelihoods', 'parameters_json']
+    with open(params_json) as json_data:
+        options_dict = json.load(json_data)
 
     # Initialise likelihood class
     likelihood_object = fsl.full_shape_spectra(options_dict)
